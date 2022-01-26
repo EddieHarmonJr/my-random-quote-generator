97a55f4fe89a6efa6c74fbd74eda872b6e921f8c
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2022-01-26 18:36:47 -0500

    comment code for future reference and ease of use for others to look at.

--- random_quote_generator_v1/js/script.js
+++ random_quote_generator_v1/js/script.js
@@ -1,31 +1,42 @@
-/************************************************
-Treehouse Techdegree:
-Project 4 - Random Quote Generator
-*************************************************/
+//Quotes creates an array to hold objects that keep quote information.
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community
-
-/*** 
- * `quotes` array 
-***/
-
-let quotes = [
-  {quote: "Hasta la vista, baby.", source: "Arnold Schwarzenegger", citation: "Terminator 2: Judgment Day", year: 1991},
-  {quote: "My mama always said life was like a box of chocolates. You never know what you're gonna get.", source: "Tom Hanks", citation: "Forrest Gump", year: 1994},
-  {quote: "Oh, I say it, and I say it again. You been had. You been took. You been hoodwinked, bamboozled, led astray, run amuck!", source: "Denzel Washington", citation: "Malcolm X", year: 1992},
-  {quote: "All my life I had to fight. I had to fight my daddy. I had to fight my uncles. I had to fight my brothers. A girl child ain’t safe in a family of mens. But I ain’t never thought I’d have to fight in my own house!", source: "Oprah Winfrey", citation: "The Color Purple", year: 1985},
-  {quote: "I guess it comes down to a simple choice, really: Get busy living, or get busy dying.", source: "Tim Robbins", citation: "The Shawshank Redemption", year: 1994}
+let quotes = [{
+    quote: "Hasta la vista, baby.",
+    source: "Arnold Schwarzenegger",
+    citation: "Terminator 2: Judgment Day",
+    year: 1991
+  },
+  {
+    quote: "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    source: "Tom Hanks",
+    citation: "Forrest Gump",
+    year: 1994
+  },
+  {
+    quote: "Oh, I say it, and I say it again. You been had. You been took. You been hoodwinked, bamboozled, led astray, run amuck!",
+    source: "Denzel Washington",
+    citation: "Malcolm X",
+    year: 1992
+  },
+  {
+    quote: "All my life I had to fight. I had to fight my daddy. I had to fight my uncles. I had to fight my brothers. A girl child ain’t safe in a family of mens. But I ain’t never thought I’d have to fight in my own house!",
+    source: "Oprah Winfrey",
+    citation: "The Color Purple",
+    year: 1985
+  },
+  {
+    quote: "I guess it comes down to a simple choice, really: Get busy living, or get busy dying.",
+    source: "Tim Robbins",
+    citation: "The Shawshank Redemption",
+    year: 1994
+  }
 ];
 
 
-/***
- * `getRandomQuote` function
-***/
+// The getRandomQuote function generates a random number from 0 to the last index in the quotes array and places that number inside the randomNumber variable. Then the function returns a quote utilizing that aforementioned variable.
 
 function getRandomQuote() {
-  let randomNumber = Math.floor( Math.random() * quotes.length - 1 ) + 1;
+  let randomNumber = Math.floor(Math.random() * quotes.length - 1) + 1;
   //console.log(randomNumber);
   //console.log( quotes[randomNumber] );
   return quotes[randomNumber];
@@ -33,9 +44,7 @@ function getRandomQuote() {
 
 //getRandomQuote();
 
-/***
- * `printQuote` function
-***/
+// The printQuote function calls the getRandomQuote function and places what it returns in a new variable called "randomQuoteObject". Another new variable, "htmlString" is created that uses template literals to take the values of the randomQuoteObject and places them in an html formatted string. If it has citation and year properties, it adds those values to the html string and then closes the string with a closing paragraph tag.
 
 function printQuote() {
   let randomQuoteObject = getRandomQuote();
@@ -43,15 +52,15 @@ function printQuote() {
   <p class="quote">${randomQuoteObject.quote}</p>
   <p class="source">${randomQuoteObject.source}
   `;
-  
+
   //console.log(randomQuoteObject.citation);
   //getRandomQuote();
 
-  if ( randomQuoteObject.hasOwnProperty('citation') ) {
+  if (randomQuoteObject.hasOwnProperty('citation')) {
     htmlString += `<span class="citation">${randomQuoteObject.citation}</span>`;
   };
 
-  if ( randomQuoteObject.hasOwnProperty('year') ) {
+  if (randomQuoteObject.hasOwnProperty('year')) {
     htmlString += `<span class="year">${randomQuoteObject.year}</span>`;
   };
 
@@ -63,6 +72,6 @@ printQuote();
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
